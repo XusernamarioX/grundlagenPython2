c30382adceddc06a4fbd0e2520471c2a7961a0be
XusernamarioX <171wita11@o365.htl-leoben.at>
2018-03-12 15:03:08 +0100

    Dieze super gemacht

--- variable.py
+++ variable.py
@@ -1,1 +1,40 @@
 # Variable
+
+aktion = input("Welche Aktion willst du? (+/-/*/:)")
+print("Aktion ", aktion, " wird ausgeführt...")
+
+if(aktion == "*"):
+    print("Multiplikation wird ausgeführt...")
+    zahl = input("Welche Zahl wählst du?")
+    zahl = int(zahl)
+    zahl2 = input("Welche Zahl wählst du?")
+    zahl2 = int(zahl2)
+    summe = zahl * zahl2
+    print("Ergebnis von ", zahl," * ", zahl2, " = ", summe)
+
+if(aktion == "+"):
+    print("Addition wird ausgeführt...")
+    zahl = input("Welche Zahl wählst du?")
+    zahl = int(zahl)
+    zahl2 = input("Welche Zahl wählst du?")
+    zahl2 = int(zahl2)
+    summe = zahl + zahl2
+    print("Ergebnis von ", zahl, " + ", zahl2, " = ", summe)
+
+if(aktion == "-"):
+    print("Minus wird ausgeführt...")
+    zahl = input("Welche Zahl wählst du?")
+    zahl = int(zahl)
+    zahl2 = input("Welche Zahl wählst du?")
+    zahl2 = int(zahl2)
+    summe = zahl - zahl2
+    print("Ergebnis von ", zahl, " - ",zahl2 ," = ", summe)
+
+if(aktion == ":"):
+    print("Division wird ausgeführt...")
+    zahl = input("Welche Zahl wählst du?")
+    zahl = int(zahl)
+    zahl2 = input("Welche Zahl wählst du?")
+    zahl2 = int(zahl2)
+    summe = zahl/zahl2
+    print("Ergebnis von ", zahl," / ", zahl2, " = ", summe)
